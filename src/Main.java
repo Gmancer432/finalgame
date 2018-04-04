9ee6ce73875e726ae562d6f2ae8dbc42ccf96f4d
Gmancer432 <sean@richens.com>
2018-04-04 13:41:12 -0600

    stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import javax.swing.*;
+
 public class Main
 {
     public static void main(String[]args) {
+        JFrame frame = new JFrame("hello");
+        frame.setSize(1000, 1000);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        Sprite sprite = new Sprite() {
+            @Override
+            public void move() {
+
+            }
+        };
+        sprite.changeImage("movinglog.png");
+        sprite.setSize(50,50);
+        sprite.setLocation(50, 50);
+        frame.add(sprite);
+
+        frame.setVisible(true);
     }
 
 }
